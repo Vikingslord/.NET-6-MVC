3b9db41fa4ea250d797519901e53d20a139f845b
Vikingslord <ahsanulhaque1998@outlook.com>
2023-02-05 09:37:56 +0600

    Final Commit

---
+++ BookStore.WebAppMVC/Data/CategoryDataContext.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.WebAppMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.WebAppMVC.Data
+{
+    public class CategoryDataContext : DbContext 
+    {
+        public CategoryDataContext(DbContextOptions<CategoryDataContext> options) : base(options)
+        {
+
+        }
+
+        //passing in Category model
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ BookStore.WebAppMVC/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.WebAppMVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now; //Adding defualt value
+    }
+}
